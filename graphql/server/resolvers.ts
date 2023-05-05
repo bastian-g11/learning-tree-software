3a0e3925530e95a723101c5cafeee6ec3c36b659
Sebastián Gómez <sgr00114@gmail.com>
2023-05-04 19:14:00 -0500

    FEAT: Add userTypes types and resolvers

--- graphql/server/resolvers.ts
+++ graphql/server/resolvers.ts
@@ -1,5 +1,6 @@
 import { Resolver } from 'types';
+import { UserTypeResolvers } from '@graphql/server/userType/resolvers';
 
-const GlobalResolvers: Resolver[] = [];
+const GlobalResolvers: Resolver[] = [UserTypeResolvers];
 
 export { GlobalResolvers };
--- graphql/server/types.ts
+++ graphql/server/types.ts
@@ -1,10 +1,11 @@
 import { DocumentNode } from 'graphql';
 import { gql } from 'apollo-server-micro';
+import { UserTypeTypes } from '@graphql/server/userType/types';
 
 const CommonTypes = gql`
   scalar Date
 `;
 
-const GlobalTypes: DocumentNode[] = [CommonTypes];
+const GlobalTypes: DocumentNode[] = [CommonTypes, UserTypeTypes];
 
 export { GlobalTypes };
