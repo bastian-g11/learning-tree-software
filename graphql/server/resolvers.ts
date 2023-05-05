092532162dc09e8b9005b18e522ca89c5bdc4a39
natogourmet <natogourmet@hotmail.com>
2023-05-04 20:30:43 -0500

    FEAT: users types and resolvers

--- graphql/server/resolvers.ts
+++ graphql/server/resolvers.ts
@@ -1,6 +1,10 @@
 import { Resolver } from 'types';
 import { UserTypeResolvers } from '@graphql/server/userType/resolvers';
+import { UserResolvers } from '@graphql/server/user/resolvers';
 
-const GlobalResolvers: Resolver[] = [UserTypeResolvers];
+const GlobalResolvers: Resolver[] = [
+  UserTypeResolvers,
+  UserResolvers
+];
 
 export { GlobalResolvers };
--- graphql/server/types.ts
+++ graphql/server/types.ts
@@ -1,11 +1,16 @@
 import { DocumentNode } from 'graphql';
 import { gql } from 'apollo-server-micro';
 import { UserTypeTypes } from '@graphql/server/userType/types';
+import { UserTypes } from '@graphql/server/user/types';
 
 const CommonTypes = gql`
   scalar Date
 `;
 
-const GlobalTypes: DocumentNode[] = [CommonTypes, UserTypeTypes];
+const GlobalTypes: DocumentNode[] = [
+  CommonTypes, 
+  UserTypeTypes,
+  UserTypes
+];
 
 export { GlobalTypes };
