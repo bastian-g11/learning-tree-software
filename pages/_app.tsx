b1b4a855e5935ff06a1fb29ac6b9e64442157ad0
natogourmet <natogourmet@hotmail.com>
2023-05-04 20:00:33 -0500

    FIX: idk?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-// import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import useApolloClient from 'hooks/useApolloClient';
+import { ApolloProvider } from '@apollo/client';
+import 'styles/globals.css';
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const MyApp = ({ Component, pageProps: { ...pageProps } }: AppProps) => {
+  const { client } = useApolloClient();
+  return (
+    <>
+      <Head>
+        <title>Page Name</title>
+        <meta name='description' content='content' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </>
+  );
+};
 
-export default App;
+export default MyApp;
